f38e590ab34e3715243e2cb193a0f2b85849fefd
d1k4s <dikaest09@gmail.com>
2022-11-27 17:46:04 +0700

    payment detail package preview

--- lib/pages/payments_detail.dart
+++ lib/pages/payments_detail.dart
@@ -31,44 +31,174 @@ class _PaymentDetailState extends State<PaymentDetail> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Padding(
-          padding: const EdgeInsets.only(top: 1),
+          padding: const EdgeInsets.only(top: 5),
           child: ListView(
             children: [
               OutlinedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0.0,
-                    shadowColor: Colors.transparent,
-                    primary: Color(0xFFFFFFFF),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                  shadowColor: Colors.transparent,
+                  primary: Color(0xFFFFFFFF),
+                ),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          scrollable: true,
+                          title: Text('Alamat Pendaftaran'),
+                          content: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Form(
+                              child: Column(
+                                children: <Widget>[
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Name',
+                                      icon: Icon(Icons.account_box),
+                                    ),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'No. Handphone',
+                                      icon: Icon(Icons.aod_rounded),
+                                    ),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Address',
+                                      icon: Icon(Icons.home),
+                                    ),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Citizenship',
+                                      icon: Icon(Icons.badge_outlined),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          actions: [
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Color(0xFF25BAC2),
+                                ),
+                                child: Text("Submit"),
+                                onPressed: () => Navigator.of(context).pop()
+                                // onPressed: () => Navigator.of(context).push(
+                                //     MaterialPageRoute(
+                                //         builder: (context) => PaymentDetail())),
+                                ),
+                          ],
+                        );
+                      });
+                },
+                icon: Icon(Icons.location_on_outlined, color: Colors.redAccent),
+                label: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Alamat Pendaftaran                                                               ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Color(0xFF303030)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Dwi Nafis Mahardika | (+62) 813-5975-2945                                ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Color(0xFF303030), fontSize: 13),
+                    ),
+                    Text(
+                      'PERUMAHAN PONDOK BAMBU BLOK O3, RT.1/RW.8 KEBON SARI,',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Color(0xFF303030), fontSize: 13),
+                    ),
+                    Text(
+                      'SUMBER SARI, KAB. JEMBER, JAWA TIMUR, INA                                 ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(color: Color(0xFF303030), fontSize: 13),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Column(children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Color(0xFFFFFFFF), width: 15)),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/ranu_kumbolo.jpg',
+                                  fit: BoxFit.cover,
+                                  height: 450, // set your height
+                                  width: MediaQuery.of(context).size.width,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "RANU KUMBOLO                                                                    ",
+                                  style: TextStyle(fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "variation : JEEP, Griya Semeru Homestay,                                 ",
+                                  style: TextStyle(
+                                      color: Colors.grey[700], fontSize: 13),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Rp 700.000                                                                            X 5   ",
+                                  style: TextStyle(
+                                      color: Colors.grey[700], fontSize: 13),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+              SizedBox(height: 5),
+              Column(
+                children: [
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFFFFFFF)),
+                    ),
                   ),
-                  onPressed: () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => PaymentDetail())),
-                  icon:
-                      Icon(Icons.location_on_outlined, color: Colors.redAccent),
-                  label: Text(
-                    'Alamat Pendaftaran',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(color: Color(0xFF303030)),
-                  )),
-              // InkWell(
-              //   child: Container(
-              //     height: 250,
-              //     width: 350,
-              //     decoration: BoxDecoration(
-              //       color: Color(0XFFFFFFFF),
-              //     ),
-              //     child: Text(
-              //       'laper bosss',
-              //       textAlign: TextAlign.left,
-              //       style: TextStyle(
-              //         fontSize: 18,
-              //         fontWeight: FontWeight.bold,
-              //       ),
-              //     ),
-              //   ),
-              //   onTap: () => Navigator.of(context).push(
-              //     MaterialPageRoute(builder: (context) => PaymentDetail()),
-              //   ),
-              // ),
+                ],
+              ),
             ],
           ),
         ),
